2f4c40cbbbaafe037de13bbaac982fd7c1154927
Rodrigo Martin Prieto <Rodrigo Martin Prieto>
2017-02-22 18:35:11 +0100

    Created player model

---
+++ app/models/player.js
@@ -0,0 +1,12 @@
+// grab the packages that we need for the player model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// player schema
+var PlayerSchema = new Schema({
+  name: { type: String, required: true, index: { unique: true }},
+  avatar_url: { type: String, required: true }
+});
+
+// return the model
+module.exports = mongoose.model('Player', PlayerSchema);
--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var mongoose 	 = require('mongoose');
 var config     = require('./config');
 var path       = require('path');
 
-var User = require('./app/models/user');
-
 mongoose.connect(config.database);
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,75 +21,7 @@ app.use(function(req, res, next) {
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
-// get an instance of the express router
-var apiRouter = express.Router();
-
-apiRouter.get('/', function(req, res) {
-	res.json({ message: 'INFERNO DIVIDER!' });
-});
-
-apiRouter.route('/users')
-	.post(function(req, res){
-		var user = new User();
-
-		user.username = req.body.username;
-		user.password = req.body.password;
-		user.level = req.body.level;
-
-		user.save(function(err){
-			if(err) {
-				if (err.code == 11000){
-					return res.json({
-						success: false,
-						message: 'User already exists'
-					})
-				} else {
-					return res.send(err);
-				}
-			}
-
-			res.json({message: 'User created!'});
-		});
-	})
-	.get(function(req, res){
-		User.find(function(err, users){
-			if (err) res.send(err);
-			res.json(users);
-		});
-	});
-
-apiRouter.route('/users/:user_id')
-	.get(function(req, res){
-		User.findById(req.params.user_id, function(err, user){
-			if (err) res.send(err);
-			res.json(user);
-		})
-	})
-	.put(function(req, res){
-		User.findById(req.params.user_id, function(err, user){
-			if (err) res.send(err);
-			if (req.body.username) user.username = req.body.username;
-			if (req.body.password) user.password = req.body.password;
-			if (req.body.level) user.level = req.body.level;
-
-			user.save(function(err){
-				if(err) res.send(err);
-				res.json({message: 'User updated!'});
-			})
-		})
-	})
-	.delete(function(req, res){
-		User.remove({
-			_id: req.params.user_id
-		}, function(err, user){
-			if (err) return res.send(err);
-			res.json({message: 'User deleted'});
-		})
-	});
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', apiRouter);
+require('./apiRoutes')(app, express);
 
 // MAIN CATCHALL ROUTE ---------------
 // SEND USERS TO FRONTEND ------------
